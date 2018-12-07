63a83b64163c07b77829db6efe2a165c0046f4ac
borgespires <borgespires@hotmail.com>
2018-12-07 22:55:47 +0000

    AoC 2018 day 4 - rename function

--- aoc_2018/day4/src/sleep_recorder.rs
+++ aoc_2018/day4/src/sleep_recorder.rs
@@ -52,7 +52,7 @@ impl SleepRecorder {
     }
 
     pub fn sleepiest_guard(&self) -> (u32, u32) {
-        fn total_sleep_time((_, time): &(u32, u32)) -> u32 {
+        fn time((_, time): &(u32, u32)) -> u32 {
             return *time;
         }
 
@@ -60,7 +60,7 @@ impl SleepRecorder {
             .records
             .keys()
             .map(|&guard| (guard, self.total_sleep_time(guard)))
-            .max_by_key(total_sleep_time)
+            .max_by_key(time)
             .unwrap();
     }
 
