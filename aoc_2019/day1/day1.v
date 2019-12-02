5f087f9cee43b28a42694c7d5c065d91ed45e879
borgespires <borgespires@hotmail.com>
2019-12-02 00:41:40 +0000

    AoC 2019 day 1

---
+++ aoc_2019/day1/day1.v
@@ -0,0 +1,36 @@
+import os
+import math
+import strconv
+
+fn main() {
+    filename := 'input.txt'
+
+    modules := os.read_lines(filename)?
+
+	println(total_fuel(modules, required_fuel)) // star 1
+	println(total_fuel(modules, rec_required_fuel)) // star 2
+}
+
+fn total_fuel(modules []string, fuel_calculator fn (int) int) int {
+	mut total := 0
+
+    for mass in modules {
+        total += fuel_calculator(strconv.atoi(mass))
+    }
+
+	return total
+}
+
+fn required_fuel(mass int) int {
+	return int(math.floor(mass / 3) - 2)
+}
+
+fn rec_required_fuel(mass int) int {
+	fuel := required_fuel(mass)
+
+	if fuel < 0 {
+		return 0
+	} else {
+		return fuel + rec_required_fuel(fuel)
+	}
+}
